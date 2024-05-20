0c7a6a1c3b4f16fb9e538ca8e7bd79f5ec2956f2
123456789aziz <168224649+123456789aziz@users.noreply.github.com>
2024-05-20 17:27:25 +0200

    Add files via upload

---
+++ eser1.js
@@ -0,0 +1,51 @@
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/
+/*  1 string: contiene principalmente del testo dentro alle virgolette
+    2 number: contiene i numeri
+    3 object: un insieme di valori
+    4 arrey: una lista di valori
+    5 boolean: che possono essere o vero o falso
+    6 null e undefined:nullo e una variabile che esiste ma non ha nessun valore dentro
+    */
+
+/* ESERCIZIO 2
+ Descrivi cos'è un oggetto in JavaScript, con parole tue.
+*/
+
+    // e una variabile che ha piu valori
+
+/* ESERCIZIO 3
+ Scriti il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+
+    /*  let number = 12 + 20;
+        console.log(number);
+ */
+
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+
+  //  let x = 12;
+
+/* ESERCIZIO 5
+ Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+
+    //  let name = "Abdou aziz ba";
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+    /* let x = 12;
+        console.log(x-=4);*/
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ Infine, verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+ NON HAI BISOGNO DI UN BLOCCO IF/ELSE. E' sufficiente utilizzare console.log().
+*/
+
